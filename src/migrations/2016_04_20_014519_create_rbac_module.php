5c0e571af3bfaeb9964d153817a61876be6e3a7c
Diego Calle <dnetix@gmail.com>
2016-04-25 18:02:27 -0500

    Migrations drafts for the models to use

---
+++ src/migrations/2016_04_20_014519_create_rbac_module.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRbacModule extends Migration
+{
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('permission');
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+
+        Schema::create('authenticatable_role', function (Blueprint $table) {
+            $table->integer('authenticatable_id')->unsigned();
+            $table->string('authenticatable_type');
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->primary(['authenticatable_id', 'authenticatable_type', 'role_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('authenticatable_role');
+        Schema::drop('permission_role');
+        Schema::drop('roles');
+    }
+}
