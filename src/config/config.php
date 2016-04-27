1ed65ea5315df12d307956119b905ba6f30adb34
Diego Calle <dnetix@gmail.com>
2016-04-27 00:18:39 -0500

    Updating the config file to accept the log_callback and the new accepted date_range attribute

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+
+    /**
+     * Allows to register a callback to log the results of the rbac operations
+     * it will return $user, $ability, $result, $arguments
+     */
+    'log_callback' => false,
+    
     'permissions' => [
         'rbac.manage.roles' => [
             'name' => 'Administrar roles de usuarios'
@@ -9,7 +16,17 @@ return [
         'rbac.assign.permissions.to.role' => [
             'name' => 'Asignar permisos a roles'
         ],
-        
+
+        /*
+         * You can provide some date range that a permission needs to fulfill in order to be
+         * granted. This date range should be provided in text with the notation used in the class
+         * \Dnetix\Dates\DateRangeChecker
+        'rbac.assign.permissions.to.role' => [
+            'name' => 'Asignar permisos a roles',
+            'date_range' => 'LV8-16|S8-12'
+        ],
+        */
+
         /*
         When a callback its provided to the permission it will be executed instead of the role checks
         The first argument always will be the authenticatable that its being checked and the others
